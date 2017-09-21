30f2ca6b988633e59168cf7d19da12489d7ab9f9
rogera.dd <wolfgang.schuessel@gmail.com>
2017-09-21 17:43:50 +0200

    naming

--- pipe-seg/nc-sample/nc_sample.sh
+++ pipe-seg/nc-sample/nc_sample.sh
@@ -14,8 +14,8 @@ date >> sample.res
 ls -al ./ | tee sample.t1.res | nc -l -p 1234 >> sample.res & # backgrounding server
 
 ## start listener
-date >> nc.rcv
-nc localhost 1234 -w 3 >> nc.rcv # timeout .. eof
+date >> nc_sample.ts.rcv
+nc localhost 1234 -w 3 >> nc_sample.rcv # timeout .. eof
 
 ## consumer: tf nc.rcv
 
